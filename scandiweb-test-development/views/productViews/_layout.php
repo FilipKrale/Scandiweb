5f20b2a340e588964c430c8f950e3ec462ffb365
FilipKrale <94227413+FilipKrale@users.noreply.github.com>
2023-09-13 20:57:22 +0200

    Add files via upload

---
+++ scandiweb-test-development/views/productViews/_layout.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--icon-->
+    <link rel="icon"
+        href="https://res.cloudinary.com/kwishi/image/upload/v1667384679/assets/ghost-icon-19-dfaa4e-16_jmri96.png" />
+    <!--css-->
+    <link rel="stylesheet" href="/public/../stylesheet/app.scss" />
+    <!--bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
+    <!--javascript-->
+    <script defer src="/public/../js/app.js"></script>
+    <title>Scandiweb - Junior Developer Task</title>
+    <style>
+    .card {
+        background-color: hsl(38, 69%, 59%);
+        box-shadow: 0 12px 24px hsl(38, 8%, 8%, 0.5);
+    }
+    </style>
+</head>
+
+<body style="background: #dfaa4e;">
+    <?php echo $content; ?>
+    
+    <footer class="footer pt-5 mt-5 py-3 text-center">
+        <div class="container-fluid">
+            <div class="h4 pb-2 mb-4 text-danger border-bottom border-dark"></div>
+            <span class="text-center mt-2">Scandiweb test assignment</span>
+        </div>
+    </footer>
+</body>
+
+</html>
